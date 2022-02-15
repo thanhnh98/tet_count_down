2d98d0e28d78b759ec32e619e85a639306d114aa
Thanh <thanh150898@gmail.com>
2022-02-15 10:35:58 +0700

    change avatar

--- buildSrc/src/main/java/AndroidConfigs.kt
+++ buildSrc/src/main/java/AndroidConfigs.kt
@@ -6,6 +6,6 @@ object AndroidConfigs {
     const val minSdkVersion = 23
     const val targetSdkVersion = 31
 
-    const val versionCode = 301
-    const val versionName = "3.0.1"
+    const val versionCode = 302
+    const val versionName = "3.0.2"
 }
